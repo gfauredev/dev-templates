d0006402ac6a96f16949b7a288ae0d966789d978
Guilhem Fauré <pro@gfaure.eu>
2025-03-05 16:42:31 +0100

    feat: add a lot of interesting fonts in typst env

--- typst/flake.nix
+++ typst/flake.nix
@@ -15,6 +15,27 @@
             typst # Advanced document processor
             tinymist # Typst LSP
             typstyle # Typst formatter
+            # Serif fonts
+            libre-baskerville # Great, stylish serif
+            vollkorn # Great serif font
+            merriweather # Serif readable on low res screens
+            gelasio # Serif Georgia replacement
+            noto-fonts-cjk-serif # Chinese, Japanese, Korean Serif
+            # Sans fonts
+            fira-go # Great sans with icons
+            nacelle # Helvetica equivalent
+            inter # Interesting sans font
+            carlito # Calibri equivalent
+            noto-fonts-cjk-sans # Chinese, Japanese, Korean Sans
+            # Mono fonts
+            nerd-fonts.fira-code
+            nerd-fonts.jetbrains-mono
+            # Packages of multiple fonts
+            liberation_ttf # ≃ Times New Roman, Arial, Courier New equivalents
+            noto-fonts # Google well internationalized fonts
+            # Symbols and emoticons
+            noto-fonts-emoji # Emojis
+            font-awesome # Icons
           ];
         };
       });

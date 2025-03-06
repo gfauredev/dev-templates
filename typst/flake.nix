2471e7845d24ff3dc164791a0e3422e4ccfed8e0
Guilhem Fauré <pro@gfaure.eu>
2025-03-06 09:29:07 +0100

    feat(typst): disable some fonts by default

--- typst/flake.nix
+++ typst/flake.nix
@@ -15,28 +15,28 @@
             typst # Advanced document processor
             tinymist # Typst LSP
             typstyle # Typst formatter
-            # Serif fonts
+            ## Serif fonts
             libre-baskerville # Great, stylish serif
             vollkorn # Great serif font
-            merriweather # Serif readable on low res screens
-            gelasio # Serif Georgia replacement
-            noto-fonts-cjk-serif # Chinese, Japanese, Korean Serif
-            # Sans fonts
+            # merriweather # Serif readable on low res screens
+            # gelasio # Serif Georgia replacement
+            # noto-fonts-cjk-serif # Chinese, Japanese, Korean Serif
+            ## Sans fonts
             fira-go # Great sans with icons
             nacelle # Helvetica equivalent
-            inter # Interesting sans font
-            carlito # Calibri equivalent
-            noto-fonts-cjk-sans # Chinese, Japanese, Korean Sans
-            # Mono fonts
-            nerd-fonts.fira-code
+            # inter # Interesting sans font
+            # carlito # Calibri equivalent
+            # noto-fonts-cjk-sans # Chinese, Japanese, Korean Sans
+            ## Mono fonts
             nerd-fonts.jetbrains-mono
-            # Packages of multiple fonts
-            liberation_ttf # ≃ Times New Roman, Arial, Courier New equivalents
-            noto-fonts # Google well internationalized fonts
-            # Symbols and emoticons
-            noto-fonts-emoji # Emojis
-            font-awesome # Icons and symbols
+            # nerd-fonts.fira-code
+            ## Packages of multiple fonts
+            # liberation_ttf # ≃ Times New Roman, Arial, Courier New equivalents
+            # noto-fonts # Google well internationalized fonts
+            ## Symbols and emoticons
             nerd-fonts.symbols-only # Even more icons and symbols
+            # font-awesome # Icons and symbols
+            # noto-fonts-emoji # Emojis
           ];
         };
       });

e4e6d140acbe9596b98903a0f7134dbd8e5334e0
Guilhem Fauré <pro@gfaure.eu>
2025-05-07 10:34:49 +0200

    fix: cadquery insecure package

--- cadquery-build123d/flake.nix
+++ cadquery-build123d/flake.nix
@@ -39,8 +39,10 @@
       let
         pkgs = import nixpkgs {
           inherit system;
-          config.permittedInsecurePackages =
-            [ "freeimage-unstable-2021-11-01" ];
+          config.permittedInsecurePackages = [
+            "freeimage-unstable-2021-11-01"
+            "freeimage-3.18.0-unstable-2024-04-18"
+          ];
         };
         # scotch = pkgs.scotch.overrideAttrs (oldAttrs: {
         #   buildFlags = [ "scotch ptscotch esmumps ptesmumps" ];

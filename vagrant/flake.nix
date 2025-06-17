300db459dfff63ee5c116c37b9f5ce562ead9825
Guilhem Fauré <pro@gfaure.eu>
2025-06-17 14:57:51 +0200

    fix: vagrant allow unfree

--- vagrant/flake.nix
+++ vagrant/flake.nix
@@ -5,8 +5,13 @@
     let
       supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
       forEachSupportedSystem = f:
-        nixpkgs.lib.genAttrs supportedSystems
-        (system: f { pkgs = import nixpkgs { inherit system; }; });
+        nixpkgs.lib.genAttrs supportedSystems (system:
+          f {
+            pkgs = import nixpkgs {
+              inherit system;
+              config.allowUnfree = true;
+            };
+          });
     in {
       devShells = forEachSupportedSystem ({ pkgs }: {
         default = pkgs.mkShell {

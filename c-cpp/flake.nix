b1233ac7e26a9f8cd2aad0337c69d6833d56a0c6
Guilhem Fauré <pro@gfaure.eu>
2025-09-26 08:29:18 +0200

    chore: clean cpp

--- c-cpp/flake.nix
+++ c-cpp/flake.nix
@@ -1,12 +1,9 @@
 {
   description = "A Nix-flake-based C/C++ development environment";
-
   inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
-
   outputs = { self, nixpkgs }:
     let
-      supportedSystems =
-        [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      supportedSystems = [ "x86_64-linux" "aarch64-linux" "aarch64-darwin" ];
       forEachSupportedSystem = f:
         nixpkgs.lib.genAttrs supportedSystems
         (system: f { pkgs = import nixpkgs { inherit system; }; });
@@ -16,7 +13,8 @@
           stdenv = pkgs.clangStdenv; # Clang instead of GCC
         } {
           packages = with pkgs; [
-            clang-tools # clang CLIs
+            # gcc # Compiler
+            clang-tools # Clang CLIs
             cmake # Automation tool
             cmake-language-server # LSP
             cppcheck # Static analysis

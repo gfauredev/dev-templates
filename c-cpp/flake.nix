b4e4c0f3c0f5a614813b70ae1f7a66def555dc3e
Guilhem Fauré <pro@guilhemfau.re>
2025-11-24 15:23:24 +0100

    feat: clean and improve c-cpp

--- c-cpp/flake.nix
+++ c-cpp/flake.nix
@@ -1,21 +1,21 @@
 {
-  description = "A Nix-flake-based C/C++ development environment";
+  description = "Nix flake C/C++ development environment";
   inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05"; # NixOS Stable
   # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   # inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0";
   outputs =
     { self, nixpkgs }:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "aarch64-darwin"
-      ];
-      forEachSupportedSystem =
-        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+      systems =
+        f:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-linux"
+          # "aarch64-darwin"
+        ] (system: f { pkgs = import nixpkgs { inherit system; }; });
     in
     {
-      devShells = forEachSupportedSystem (
+      devShells = systems (
         { pkgs }:
         {
           default =
@@ -25,29 +25,24 @@
               }
               {
                 packages = with pkgs; [
-                  # gcc # Compiler
-                  # conan # C++ package manager
                   clang-tools # Clang CLIs
                   cmake # Automation tool
                   cmake-language-server # LSP
                   cppcheck # Static analysis
-                  clang-uml # Generate UML from c(++) TEST it
+                  # clang-uml # Generate UML from c(++)
                   doctest # Testing framework
                   doxygen # Documentation generator
-                  gdb # Debugger
-                  gnumake # Automation tool
-                  gtest # Testing framework
+                  # gdb # Debugger
+                  # gtest # Testing framework
                   lcov # Code coverage
                   lldb # Debug adapter
-                  pkg-config # Find libraries
+                  # pkg-config # Find libraries
                   valgrind # Debugging and profiling
                 ];
                 env = {
                   CXX = "c++"; # Use Clang++ as the default C++ compiler
                   CC = "clang"; # Use Clang as the default C compiler
-                  CXXFLAGS = ''
-                    -std=c++23 -Wall -Wextra -Wpedantic -Wshadow -Wconversion
-                  ''; # C++23 and more warnings
+                  CXXFLAGS = "-std=c++23 -Wall -Wextra -Wpedantic -Wshadow -Wconversion";
                 };
               };
         }

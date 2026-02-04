63d1c9dc6cadff793690a18aeb054b05da61201d
Guilhem Fauré <pro@guilhemfau.re>
2026-02-04 12:57:35 +0100

    chore: clean web

--- web/flake.nix
+++ web/flake.nix
@@ -1,30 +1,31 @@
 {
-  description = "A Nix-flake-based web development environment";
-  # inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  description = "Nix-flake web development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  # inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
   outputs =
     { self, nixpkgs }:
     let
-      supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-      ];
-      forEachSupportedSystem =
-        f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });
+      systems =
+        f:
+        nixpkgs.lib.genAttrs [
+          "x86_64-linux"
+          "aarch64-darwin"
+          "aarch64-linux"
+        ] (system: f { pkgs = import nixpkgs { inherit system; }; });
     in
     {
-      devShells = forEachSupportedSystem (
+      devShells = systems (
         { pkgs }:
         {
           default = pkgs.mkShell {
             packages = with pkgs; [
+              biome
+              bun # Faster runtime, bundler, package manager
+              taplo # TOML LSP
               typescript
               typescript-language-server # TS LSP
-              biome
               vscode-langservers-extracted # HTML/CSS/JS(ON)
               yaml-language-server # YAML LSP
-              taplo # TOML LSP
-              bun # Faster runtime, bundler, package manager
               # deno # More secure runtime
               # node2nix
               # nodejs

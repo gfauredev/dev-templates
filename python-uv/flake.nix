b475e5f9687929c41a9315668ba6dfa621231b69
Guilhem Fauré <pro@guilhemfau.re>
2026-05-11 14:11:46 +0200

    fix: nixos unstable python

--- python-uv/flake.nix
+++ python-uv/flake.nix
@@ -1,14 +1,16 @@
 {
   description = "A Nix-flake-based Python development environment";
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
+  nixConfig = {
+    extra-substituters = [ "https://cache.garnix.io" ];
+    extra-trusted-public-keys = [ "cache.garnix.io:CTFPyKSLcx5RMJKfLo5EEPUObbA78b0YQ2DTCJXqr9g=" ];
+  };
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  # inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1.*.tar.gz";
   outputs =
     { self, nixpkgs }:
     let
       supportedSystems = [
-        "x86_64-linux"
-        "aarch64-linux"
-        "x86_64-darwin"
-        "aarch64-darwin"
+        "x86_64-linux" # "aarch64-linux" # "x86_64-darwin" # "aarch64-darwin"
       ];
       forEachSupportedSystem =
         f: nixpkgs.lib.genAttrs supportedSystems (system: f { pkgs = import nixpkgs { inherit system; }; });

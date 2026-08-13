9783e1faffde5c658835267a6c5b734195f0d993
Guilhem Fauré <pro@guilhemfau.re>
2026-08-13 14:59:57 +0200

    feat: bun env

--- bun/flake.nix
+++ bun/flake.nix
@@ -1,33 +1,98 @@
 {
-  description = "A Nix-flake-based Bun development environment";
-
-  inputs.nixpkgs.url = "https://flakehub.com/f/NixOS/nixpkgs/0.1"; # unstable Nixpkgs
-
+  description = "Bun/Astro development environment";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
   outputs =
-    { self, ... }@inputs:
-
+    { self, nixpkgs }:
     let
-      supportedSystems = [
+      forSystems = nixpkgs.lib.genAttrs [
         "x86_64-linux"
         "aarch64-linux"
-        "x86_64-darwin"
         "aarch64-darwin"
       ];
-      forEachSupportedSystem =
-        f:
-        inputs.nixpkgs.lib.genAttrs supportedSystems (
-          system:
-          f {
-            pkgs = import inputs.nixpkgs { inherit system; };
-          }
-        );
+      nixpkgsFor = forSystems (system: import nixpkgs { inherit system; });
     in
     {
-      devShells = forEachSupportedSystem (
-        { pkgs }:
+      packages = forSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+          cleanSrc = pkgs.nix-gitignore.gitignoreSourcePure [ ./.gitignore ] ./.;
+        in
         {
-          default = pkgs.mkShellNoCC {
-            packages = with pkgs; [ bun ];
+          nodeDeps = pkgs.stdenv.mkDerivation {
+            pname = "node_modules";
+            version = "0.1.0";
+            src = cleanSrc;
+            nativeBuildInputs = [ pkgs.bun ];
+            preBuild = ''
+              export HOME=$(mktemp -d)
+            '';
+            buildPhase = ''
+              bun install --no-progress --frozen-lockfile
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp -R ./node_modules $out/node_modules
+              rm -rf $out/node_modules/.cache
+            '';
+            outputHashMode = "recursive";
+            outputHashAlgo = "sha256";
+            outputHash = pkgs.lib.fakeHash;
+          };
+          site = pkgs.stdenv.mkDerivation {
+            pname = "app";
+            version = "0.1.0";
+            src = cleanSrc;
+            nativeBuildInputs = [
+              pkgs.bun
+              pkgs.nodejs
+            ];
+            configurePhase = ''
+              cp -r ${self.packages.${system}.nodeDeps}/node_modules ./node_modules
+              chmod -R +w ./node_modules
+              patchShebangs ./node_modules
+            '';
+            doCheck = true;
+            checkPhase = ''
+              export HOME=$(mktemp -d)
+              export ASTRO_TELEMETRY_DISABLED=1
+              bun run check
+            '';
+            buildPhase = ''
+              export HOME=$(mktemp -d)
+              export ASTRO_TELEMETRY_DISABLED=1
+              bun run build
+            '';
+            installPhase = ''
+              mkdir -p $out
+              cp -r dist/* $out/
+            '';
+          };
+          default = self.packages.${system}.site;
+        }
+      );
+      devShells = forSystems (
+        system:
+        let
+          pkgs = nixpkgsFor.${system};
+        in
+        {
+          default = pkgs.mkShell {
+            inputsFrom = [ self.packages.${system}.default ];
+            packages = with pkgs; [
+              astro-language-server # LSP
+              jsonfmt
+              jq
+              prettier
+              taplo # TOML LSP
+              typescript-language-server # TS LSP
+              vscode-langservers-extracted # HTML/CSS/JS(ON)
+              yaml-language-server # YAML LSP
+            ];
+            shellHook = ''
+              export ASTRO_TELEMETRY_DISABLED=1 # Problems with sandboxing
+              echo "🚀 Astro dev environment loaded (run 'bun install')"
+            '';
           };
         }
       );
